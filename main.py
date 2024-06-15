d42518b3b9267b457ca1885d92e11b53e05c0347
Joao Pedro S <joaopedrojp20083@gmail.com>
2024-06-15 10:20:34 -0300

    Added an UI with a button to generate the csv delta file.

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
+import os.path
 from difflib import Differ
 import pandas as pd
+from functions import load_and_compare
+import tkinter as tk
+from tkinter import filedialog
 
-def load_and_compare(basefile_loc, otherfile_loc):
-    base_file = pd.read_excel(basefile_loc)
-    otherfile = pd.read_excel(otherfile_loc)
+file_path = []
+def select_and_store_file():
+    global file_path
+    selected_file = filedialog.askopenfilenames()
+    if selected_file:
+        try:
+            file_path = list(selected_file)
+            print("Paths: ", file_path)
+            load_and_compare(file_path[0], file_path[1])
+        except Exception as e:
+            print(f"Reading file loc error: {e}")
 
-    # Sort the DataFrames by 'has pipeline' and 'onboarded'
-    df1_sorted = base_file.sort_values(by=['has pipeline', 'onboarded'])
-    df2_sorted = otherfile.sort_values(by=['has pipeline', 'onboarded'])
 
-    # Reset index to ensure proper alignment for comparison
-    df1_sorted.reset_index(drop=True, inplace=True)
-    df2_sorted.reset_index(drop=True, inplace=True)
 
-    d = Differ()
-    df1_str = df1_sorted.to_csv(index=False).splitlines()
-    df2_str = df2_sorted.to_csv(index=False).splitlines()
+main_window = tk.Tk()
+main_window.title("DiffProj - Main Window")
 
-    difference = d.compare(df1_str, df2_str)
+basefile_btn = tk.Button(main_window, text="Select the files (first the base and later the other one)", command=select_and_store_file)
+basefile_btn.pack()
 
-    diff_rows = [line for line in difference if line.startswith('+ ') or line.startswith('- ')]
-    delta_df = pd.DataFrame(diff_rows, columns=["Difference"])
-
-    delta_df.to_csv('delta.csv', index=False)
-    print("Delta between the two files has been written to 'delta.csv'.")
-
-load_and_compare("C:/Users/joaop/Downloads/reportbase.xlsx", "C:/Users/joaop/Downloads/reportmarch.xlsx")
+main_window.mainloop()
